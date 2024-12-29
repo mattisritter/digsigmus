50187252ccb6a5a99d7458e3428658161ac26e98
labor <mattis.ritter@gmail.com>
2024-12-29 13:36:22 +0100

    correct t

--- project copy.py
+++ project copy.py
@@ -12,12 +12,11 @@ from add import add
 samples = 64
 t = np.linspace(0, 2*pi, samples, endpoint=False)
 fcn = 3 + np.cos(t+1) + 2*np.cos(3*t+2) - 5*np.cos(4*t-1) + np.cos(13*t)
-f1 = Function(range(samples), Ts=1, f=fcn)
+f1 = Function(range(samples), Ts=2*pi/samples, f=fcn)
 fcn = (t % pi)*3
-f2 = Function(range(samples), Ts=1, f=fcn)
+f2 = Function(range(samples), Ts=2*pi/samples, f=fcn)
 # Low-pass filter the functions
-cut_off = 7 # times base frequency
-wc = 2*pi/(samples/cut_off) # Cut-off frequency
+wc = 7 # Cut-off frequency
 N = 15 # Filter length
 f1_low_pass = low_pass_filter(f1, wc, N)
 f2_low_pass = low_pass_filter(f2, wc, N)
@@ -26,21 +25,17 @@ factor = 1 # remove when sampling rate conversion is implemented
 if factor != 1:
     N = N*factor
     samples = samples*factor
-    f1_low_pass = convert_sampling_rate(f1_low_pass, 15, Ts_new=1/factor)
-    f2_low_pass = convert_sampling_rate(f2_low_pass, 15, Ts_new=1/factor)
+    f1_low_pass = convert_sampling_rate(f1_low_pass, 15, Ts_new=f1.Ts/factor)
+    f2_low_pass = convert_sampling_rate(f2_low_pass, 15, Ts_new=f2.Ts/factor)
 # Modulate the functions
 use_quadrature = False
 if not use_quadrature:
-    mod1 =  8 # times base frequency
-    w_mod1 = 2*pi/(samples/mod1)*factor # Modulation frequency
-    mod2 = 23 # times base frequency
-    w_mod2 = 2*pi/(samples/mod2)*factor # Modulation frequency
+    w_mod1 = 8 # Modulation frequency
+    w_mod2 = 19 # Modulation frequency
     f1_mod = modulate(f1_low_pass, w_mod1)
     f2_mod = modulate(f2_low_pass, w_mod2)
 else: # Quadrature modulation
-    mod1 =  8 # times base frequency
-    mod2 = mod1
-    w_mod = 2*pi/(samples/mod1)*factor # Modulation frequency
+    w_mod = 8 # Modulation frequency
     f1_mod, f2_mod = quadrature_modulate(f1_low_pass, f2_low_pass, w_mod)
 # Add the modulated functions
 f_sum = add(f1_mod, f2_mod)
@@ -98,17 +93,23 @@ axs[0,1].scatter(range(int(samples/2/factor)), np.abs(f2_fft[:int(samples/2/fact
 axs[0,1].set_title('Fourier Coefficients of the functions')
 axs[1,1].scatter(range(int(samples/2)), np.abs(f1_low_pass_fft[:int(samples/2)]))
 axs[1,1].scatter(range(int(samples/2)), np.abs(f2_low_pass_fft[:int(samples/2)]))
-axs[1,1].axvline(x=cut_off, color='k', linestyle='--', label='Cut-off frequency') # Add vertical line at cut-off frequency
-axs[1,1].text((cut_off+1)/31, 0.5, '$\omega_c$', transform=axs[1,1].transAxes, fontsize=10, verticalalignment='top', horizontalalignment='left')
+axs[1,1].axvline(x=wc, color='k', linestyle='--', label='Cut-off frequency') # Add vertical line at cut-off frequency
+axs[1,1].text(8.3/32, 0.5, '$\omega_c$', transform=axs[1,1].transAxes, fontsize=10, verticalalignment='top', horizontalalignment='left')
 axs[1,1].set_title('Fourier Coefficients of the low-pass filtered functions')
 axs[2,1].scatter(range(int(samples/2)), np.abs(f1_mod_fft[:int(samples/2)]))
 axs[2,1].scatter(range(int(samples/2)), np.abs(f2_mod_fft[:int(samples/2)]))
-axs[2,1].axvline(x=mod1, color='k', linestyle='--') # Add vertical line at modulation frequency
-axs[2,1].text((mod1+1)/31, 0.5, '$\omega_{mod_1}$', transform=axs[2,1].transAxes, fontsize=10, verticalalignment='top', horizontalalignment='left')
-axs[2,1].fill_between([mod1-cut_off, mod1+cut_off], 0, max(np.abs(f1_mod_fft[:int(samples/2)])), color='tab:blue', alpha=0.3) # Add a sqaure showing the frequency band
-axs[2,1].axvline(x=mod2, color='k', linestyle='--') # Add vertical line at modulation frequency
-axs[2,1].text((mod2)/32, 0.6, '$\omega_{mod_2}$', transform=axs[2,1].transAxes, fontsize=10, verticalalignment='top', horizontalalignment='left')
-axs[2,1].fill_between([mod2-cut_off, mod2+cut_off], 0, max(np.abs(f2_mod_fft[:int(samples/2)])), color='tab:orange', alpha=0.3) # Add a sqaure showing the frequency band
+if not use_quadrature:
+    axs[2,1].axvline(x=w_mod1, color='k', linestyle='--') # Add vertical line at modulation frequency
+    axs[2,1].text(9.2/32, 0.5, '$\omega_{mod_1}$', transform=axs[2,1].transAxes, fontsize=10, verticalalignment='top', horizontalalignment='left')
+    axs[2,1].fill_between([w_mod1-wc, w_mod1+wc], 0, max(np.abs(f1_mod_fft[:int(samples/2)])), color='tab:blue', alpha=0.3) # Add a sqaure showing the frequency band
+    axs[2,1].axvline(x=w_mod2, color='k', linestyle='--') # Add vertical line at modulation frequency
+    axs[2,1].text(19.2/32, 0.6, '$\omega_{mod_2}$', transform=axs[2,1].transAxes, fontsize=10, verticalalignment='top', horizontalalignment='left')
+    axs[2,1].fill_between([w_mod2-wc, w_mod2+wc], 0, max(np.abs(f2_mod_fft[:int(samples/2)])), color='tab:orange', alpha=0.3) # Add a sqaure showing the frequency band
+else:
+    axs[2,1].axvline(x=w_mod, color='k', linestyle='--')
+    axs[2,1].text(8.5/16, 0.5, '$\omega_{mod}$', transform=axs[2,1].transAxes, fontsize=10, verticalalignment='top', horizontalalignment='left')
+    axs[2,1].fill_between([w_mod-wc, w_mod+wc], 0, max(np.abs(f1_mod_fft[:int(samples/2)])), color='tab:blue', alpha=0.3)
+    axs[2,1].fill_between([w_mod-wc, w_mod+wc], 0, max(np.abs(f2_mod_fft[:int(samples/2)])), color='tab:orange', alpha=0.3)
 axs[2,1].set_title('Fourier Coefficients of the modulated functions')
 axs[3,1].scatter(range(int(samples/2)), np.abs(f_sum_fft[:int(samples/2)]), color='tab:green')
 axs[3,1].set_title('Fourier Coefficients of the sum')
@@ -126,10 +127,10 @@ plt.show()
 
 # Plot low original vs reconstructed signals
 # shift signals to align
-if False:
+if True:
     t1_lp = [t_i - (N)*f1_demod_low_pass.Ts for t_i in f1_demod_low_pass.t]
     t2_lp = [t_i - (N)*f2_demod_low_pass.Ts for t_i in f2_demod_low_pass.t]
-    fig, axs = plt.subplots(1, 2, figsize=(10, 10))
+    fig, axs = plt.subplots(1, 2, figsize=(10, 3))
     axs[0].plot(f1_low_pass.t[N:samples+N], f1_low_pass.f[N:samples+N])
     axs[0].plot(t1_lp[2*N:samples+2*N], f1_demod_low_pass.f[2*N:samples+2*N], color='darkblue')
     axs[0].set_xlabel('t [s]')
