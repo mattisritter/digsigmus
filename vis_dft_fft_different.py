6fb518ece8cad4d09e7c61da70bb4606f470761b
JakobKurz99 <jakuhss18@gmail.com>
2024-12-30 13:50:01 +0100

    Documenation and Test-File

--- vis_dft_fft_different.py
+++ vis_dft_fft_different.py
@@ -92,6 +92,10 @@ def visualize_differences():
         fft_recursive_time_usage.append(fft_recursive_time)
         fft_iterative_time_usage.append(fft_iterative_time)
 
+        # Break if iterative FFT computing time exceeds recursive FFT 
+        if fft_iterative_time < fft_recursive_time:
+            break
+
     # Plot memory usage over signal lengths
     plt.figure(figsize=(12, 6))
     plt.plot(durations[:len(dft_memory_usage)], np.array(dft_memory_usage) / 1024, label='DFT', marker='o', linestyle='-', color='blue')
